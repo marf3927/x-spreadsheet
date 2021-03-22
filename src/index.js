fcec5b695e48db20b5753c347bb43b8754759e23
marf3927 <marf3927@gmail.com>
2021-03-22 09:57:22 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Sheet from './component/sheet';
 import Bottombar from './component/bottombar';
 import { cssPrefix } from './config';
 import { locale } from './locale/locale';
-import './index.less';
 
 
 class Spreadsheet {
